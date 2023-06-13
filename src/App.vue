26cc8d3a505a6ff89a1b50849d3b6a28183fa1cb
DamianoCasolari <damianocasolari@gmail.com>
2023-06-13 18:47:50 +0200

    add info from api to front-page

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,8 @@ export default {
             base_URL: 'http://127.0.0.1:8000/',
             projects_API: 'api/projects',
             loading: true,
-            projects: []
+            projects: [],
+            error: null
         }
 
     },
@@ -20,7 +21,13 @@ export default {
         getProjects(url) {
             axios.get(url).then(response => {
                 console.log(response);
-            }).catch(error => console.log(error))
+                this.projects = response.data.projects
+                this.loading = false
+            }).catch(error => {
+                console.log(error)
+                this.error = error.message
+            }
+            )
         }
     },
     mounted() {
@@ -33,8 +40,21 @@ export default {
 <template>
     <section class="vue-home">
         <div class="container">
-            <div class="row">
-                vlog
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">
+                <div class="col" v-for="project in projects">
+                    <div class="card" style="width:18rem;">
+
+                        <!-- <img src="https://images.unsplash.com/photo-1561154464-82e9adf32764?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+                        class="card-img-top" alt="..."> -->
+                        <div class="card-body">
+                            <h5 class="card-title">{{ project.title }}</h5>
+                            <a href="{{ project.link }}">
+                                <h6 class="card-subtitle mb-2 text-muted ">{{ project.link }}</h6>
+                            </a>
+                            <p class="card-text">{{ project.functionality }}.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
