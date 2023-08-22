3a51969bc0e69be467ccd42067ecb53a7d5fe857
DamianoCasolari <damianocasolari@gmail.com>
2023-08-22 16:24:02 +0200

    change url for axios in singlepage & site main

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -10,7 +10,7 @@ export default {
 
     data() {
         return {
-            base_URL: 'http://127.0.0.1:8000/',
+            base_URL: 'http://back.damianocasolari.com',
             projects_API: 'api/projects',
             loading: true,
             projects: [],
