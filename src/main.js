8a732e07b78b973777be78a0dd09e6e8482c4b52
DamianoCasolari <damianocasolari@gmail.com>
2024-12-01 17:07:02 +0100

    Add google analystic

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
-import { createApp } from 'vue'
-import './styles/general.scss'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './styles/general.scss';
+import App from './App.vue';
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
-import { router } from './router.js'
-createApp(App).use(router).mount('#app')
+import * as bootstrap from 'bootstrap';
+import { router } from './router.js';
+
+const app = createApp(App);
+
+// Tracciamento delle viste di pagina con Google Analytics
+router.afterEach((to) => {
+    if (window.gtag) {
+        window.gtag('config', 'G-CDZPGX7L7Z', {
+            page_path: to.fullPath,
+        });
+    }
+});
+
+// Usa il router e monta l'app
+app.use(router).mount('#app');
+
